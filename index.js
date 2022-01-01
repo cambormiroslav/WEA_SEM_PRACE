f195738a0e167391b1c559e3429c6267bda663d1
miroslav.cambor <miroslav.cambor@tul.cz>
2022-01-01 14:34:03 +0100

    rename login

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.use(express.static(__dirname));
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname+'/index.html');
+    res.sendFile(__dirname+'/login.html');
 });
 
 app.listen(process.env.PORT || 5555);
