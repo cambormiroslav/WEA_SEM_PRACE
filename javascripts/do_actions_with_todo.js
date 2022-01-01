d865112cb51d352389c99a2c8e62563057ccc52d
miroslav.cambor <miroslav.cambor@tul.cz>
2022-01-01 14:04:10 +0100

    add edit button

--- javascripts/do_actions_with_todo.js
+++ javascripts/do_actions_with_todo.js
@@ -1,15 +1,24 @@
 var node_list = document.getElementsByTagName('LI');
 
 for (var i = 0; i < node_list.length; i++){
+    //close button
     var span = document.createElement('SPAN');
     var txt = document.createTextNode('\u00D7');
     span.className='close';
+    span.appendChild(txt);
+    node_list[i].appendChild(span);
+
+    //edit button
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode('\u270E');
+    span.className='edit';
     span.appendChild(txt);
     node_list[i].appendChild(span);
 }
 
 var close_it = document.getElementsByClassName('close');
 
+//close reminder = display: none
 for(var i = 0; i < close_it.length; i++){
     close_it[i].onclick=function(){
         var div = this.parentElement;
@@ -17,6 +26,7 @@ for(var i = 0; i < close_it.length; i++){
     }
 }
 
+//do reminder done
 var list = document.querySelector('ul')
 list.addEventListener('click', function(ev){
     if(ev.target.tagName === 'LI'){
@@ -25,6 +35,7 @@ list.addEventListener('click', function(ev){
 },false)
 
 function newElement(){
+    //create new li element
     var li = document.createElement('li');
     var inputValue = document.getElementById("item").value;
     var t = document.createTextNode(inputValue);
@@ -35,14 +46,24 @@ function newElement(){
         document.getElementById('list').appendChild(li);
     }
 
+    //get value for new reminder
     document.getElementById('item').value='';
 
+    //add close button
     var span = document.createElement('SPAN');
     var txt = document.createTextNode('\u00D7');
     span.className='close';
     span.appendChild(txt);
     li.appendChild(span);
 
+    //add edit button
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode('\u270E');
+    span.className='edit';
+    span.appendChild(txt);
+    li.appendChild(span);
+    
+    //close reminder = display: none
     var close_it = document.getElementsByClassName('close');
 
     for(var i = 0; i < close_it.length; i++){
