695099f21d546f1c1fc48c69d70443cc1fc50076
miroslav.cambor <miroslav.cambor@tul.cz>
2022-01-01 12:35:56 +0100

    zk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    res.sendFile("index.html");
+    res.send("index.html");
 });
 
 app.listen(process.env.PORT || 5555);
