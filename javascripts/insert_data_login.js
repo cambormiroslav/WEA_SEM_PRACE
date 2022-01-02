ea23f712859adc32e93a17a0b843c26b31ab98ac
miroslav.cambor <miroslav.cambor@tul.cz>
2022-01-02 21:57:15 +0100

    delete one row

--- javascripts/insert_data_login.js
+++ javascripts/insert_data_login.js
@@ -1,5 +1,4 @@
 const sqlite3 = require("sqlite3").verbose();
-const path = require('path');
 
 const db = new sqlite3.Database("./databases/data.db", sqlite3.OPEN_READWRITE, (err) => {
     if(err) return console.log(err.message);
